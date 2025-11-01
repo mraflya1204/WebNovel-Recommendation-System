2830ada27747da31e23f76f2f6148c94585a6b86
Muhammad Rafly Abdillah <150163424+mraflya1204@users.noreply.github.com>
2025-11-01 15:16:04 +0700

    Added Related Works, rename main.py to constructor.py

--- helper.py
+++ helper.py
@@ -1,21 +1,49 @@
-def parseStringList(s):
-    if not s or not s.startswith('[') or not s.endswith(']'):
-        return []
-    
-    sStripped = s.strip("[]")
-    parts = sStripped.split("'")
-    
-    items = [part.strip() for part in parts if part.strip()]
-    return items
+import ast
+import re
 
-def createRelationships(tx, title, author, genreString, tagString):
+# Associated Works need its own function because the data has complex format
+def parseAssociatedWorks(s):
+    """
+    Parses the complex 'related_series' string using regex
+    to extract all novel titles.
+    """
+    if not s:
+        return []
+    try:
+        titles = re.findall(r"'title':\s*'((?:\\'|[^'])*)'", s)
+        
+        return [title.strip() for title in titles]
+    
+    except Exception as e:
+        print(f"Warning: Could not parse associated works with regex: {s} | Error: {e}")
+        return []
+
+def parseStringList(s):
+    if not s:
+        return []
+        
+    try:
+        items = ast.literal_eval(s)
+        
+        if not isinstance(items, list):
+            return []
+        
+        return [str(item).strip() for item in items]
+        
+    except (ValueError, SyntaxError):
+        print(f"Warning: Could not parse string list: {s}")
+        return []
+
+def createRelationships(tx, title, authorString, genreString, tagString, associatedWorksString):
     genreList = parseStringList(genreString)
     tagList = parseStringList(tagString)
+    authorList = parseStringList(authorString) 
+    associatedWorks = parseAssociatedWorks(associatedWorksString) 
     
     query = """
     MERGE (n:Novel {name: $title})
     
-    FOREACH (authorName IN CASE WHEN $author IS NOT NULL AND $author <> "" THEN [$author] ELSE [] END |
+    FOREACH (authorName IN $authorList |
         MERGE (a:Author {name: authorName})
         MERGE (n)-[:WrittenBy]->(a)
     )
@@ -29,13 +57,19 @@ def createRelationships(tx, title, author, genreString, tagString):
         MERGE (t:Tag {name: tagName})
         MERGE (n)-[:HasTag]->(t)
     )
+
+    FOREACH (workTitle IN $associatedWorks |
+        MERGE (aw:Novel {name: workTitle})
+        MERGE (n)-[:AssociatedWith]->(aw)
+    )
     
     RETURN n.name
     """
 
     tx.run(query, 
            title=title, 
-           author=author, 
+           authorList=authorList,  
            genreList=genreList, 
-           tagList=tagList
+           tagList=tagList,
+           associatedWorks=associatedWorks
     )
